5637cff615e4e6de0a2378b8552247ae1a5d4957
Ragunathan96 <itzzmeraghu@gmail.com>
2017-07-03 17:04:17 +0530

    Add files via upload

---
+++ Chochalate61.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class Chochalate61 {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int m=s.nextInt();
+		int n=s.nextInt();
+		System.out.println(m*n-1);
+	}
+
+}
